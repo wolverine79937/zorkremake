5896311f2ba200e548173211a4a5c088594585c1
Glotón <abbotyuri@gmail.com>
2023-05-08 14:15:49 -0600

    Working on a text parser with a parser prompt that stays at the bottom of the console and takes cardinal directions and a few other things along the way.

--- Source/proto.cs
+++ Source/proto.cs
@@ -1,70 +1,8 @@
-﻿/* This is my original source. I didn't want to dump it just yet.
- * using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        // Set up the console window
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.Clear();
-        int totalWidth = Console.WindowWidth;
-        int columnWidth = (totalWidth - 2) / 3;
-        int score = 0;
-        int moves = 0;
-
-        // Initialize variables for status line
-        string tScore = "$Score: " + score;
-        string title = "The Bio Game";
-        string tMoves = "Moves: " + moves;
-
-        // Set up the status bar at the top of the console window
-        Console.SetCursorPosition(0, 0);
-        Console.BackgroundColor = ConsoleColor.DarkGray;
-        Console.ForegroundColor = ConsoleColor.Black;
-        string status = tScore.PadRight(columnWidth) + " " + title.PadRight(columnWidth) + " " + tMoves.PadRight(columnWidth);
-        Console.WriteLine(status);
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-
-        // Prompt the user for their name
-        Console.SetCursorPosition(0, 2);
-        Console.Write($"The current width of this console is: { totalWidth}\nWhat is your name? ");
-        string name = Console.ReadLine();
-
-        // Prompt the user for their age
-        Console.SetCursorPosition(0, 4);
-        Console.WriteLine("How old are you? ");
-        int age = int.Parse(Console.ReadLine());
-        score += 1;
-        status = tScore.PadRight(columnWidth) + " " + title.PadRight(columnWidth) + " " + tMoves.PadRight(columnWidth);
-
-        // Update the status bar with the player's initial score
-        Console.SetCursorPosition(0, 0);
-        Console.BackgroundColor = ConsoleColor.DarkGray;
-        Console.ForegroundColor = ConsoleColor.Black;
-        Console.Write(status);
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-
-        // Set the console colors back to gray and white
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-
-        // Print a personalized message
-        Console.SetCursorPosition(0, 6);
-        Console.WriteLine($"Hello, {name}! You are {age} years old.\nBy the way, your score just went up 1 point.");
-        Console.ReadLine();
-    }
-}
-*/
-
-using System;
-
-class Program
-{
-    static void Main()
     {
         // Here we will initializ ethe integer variables for score and moves.
         int score = 0;
@@ -74,37 +12,25 @@ class Program
         UpdateStatusBar(score, moves);
 
         // This is where the game loop will eventually goe.
-        // REMEMBER: Whenever we upchange the score or number of moves, we have to call UpdateStatusBar()
+        // REMEMBER: Whenever we change the score or number of moves, we have to call UpdateStatusBar()
+        Console.SetCursorPosition(0, 3);
+        Console.Write("ZORK I: The Great Underground Empire" + "\n" + "Copyright (c) 1981, 1982, 1983 Infocom, Inc. All rights reserved."
+                      + "\n" + "ZORK is a registered trademark of Infocom, Inc." + "\n" + "Revision 99 / Serial number 501724" + "\n"
+                      + "This copy of the game is a hobby remake." + "\n" + "All intelectual material used by this programmer is copyrighted by Infocom." + "\n");
 
-        // And now we will make an example of updating the score and moves.
-        //        score += 10;
-        //      moves += 1;
-        //    UpdateStatusBar(score, moves);
+        while (true)
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 1); // Set cursor position to the last row
+            Console.Write("> "); // And display the input prompt. It looks funny if you don't include the input line next.
 
-        // We are going to prompt the user for their name:
-        Console.SetCursorPosition(0, 2);
-        Console.Write("What is your name? ");
-        string name = Console.ReadLine();
+            string input = Console.ReadLine();
 
-        // Here, we will update the move snad score by 1 each.
-        score += 1;
-        moves += 1;
-        UpdateStatusBar(score, moves);
-
-        // For now, we still have to move the cursor manually. This is going to be fixed in the near future.
-        Console.SetCursorPosition(0, 4);
-        Console.Write("How old are you? ");
-        int age = int.Parse(Console.ReadLine());
-
-        // Now, we are going to set the score to 100 points for solving the game in 2 moves.
-        score = 100;
-        moves += 1;
-        UpdateStatusBar(score, moves);
-
-        Console.SetCursorPosition(0, 6);
-
-        // Now to tkeep the console open until we press a key to close it.
-        Console.ReadKey(true);
+            // Clear the previous output before displaying new output
+            Console.SetCursorPosition(0, 3); // set cursor position to row 3
+            Console.Write(new string(' ', Console.WindowWidth)); // This clears row 3
+            Console.SetCursorPosition(0, 4); // And now we move the cursor to row 4 of the console
+            Console.Write(new string(' ', Console.WindowWidth)); // Clear row 4.
+        }
     }
 
     static void UpdateStatusBar(int score, int moves)
@@ -121,12 +47,11 @@ class Program
 
         // Now we write the name of the game in the center column:
         Console.SetCursorPosition(Console.WindowWidth / 2 - 5, 0);
-        Console.Write("The BIO Game");
+        Console.Write("Zork I: The Great Underground Empire");
 
         // Finally, we write the number of moves in the right column
         Console.SetCursorPosition(Console.WindowWidth - 16, 0);
-        Console.Write("Moves: " + moves.ToString().PadLeft(5));
-
+        Console.Write("Moves: " + moves.ToString().PadLeft(5) + "\n");
         // Reset the console colors
         Console.BackgroundColor = ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.Gray;
