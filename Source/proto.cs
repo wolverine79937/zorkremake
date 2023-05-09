23a9d8c2644b02f49aa0174dd8961e90a1adc5c6
Glotón <abbotyuri@gmail.com>
2023-05-09 13:20:06 -0600

    It's a shame I can be so anal. I got things working the way that I want them to for the moment. The intiial intro text wasn't disappearing, so I rewrote that part of the program so that it does disappear and the game buffer begins.

--- Source/proto.cs
+++ Source/proto.cs
@@ -16,33 +16,40 @@ class Program
 
         // This is where the game loop will eventually goe.
         // REMEMBER: Whenever we change the score or number of moves, we have to call UpdateStatusBar()
-        Console.SetCursorPosition(0, 3);
+        Console.SetCursorPosition(0, 1);
         Console.Write("ZORK I: The Great Underground Empire" + "\n" + "Copyright (c) 1981, 1982, 1983 Infocom, Inc. All rights reserved."
                       + "\n" + "ZORK is a registered trademark of Infocom, Inc." + "\n" + "Revision 99 / Serial number 501724" + "\n"
-                      + "This copy of the game is a hobby remake." + "\n" + "All intelectual material used by this programmer is copyrighted by Infocom." + "\n");
+                      + "This copy of the game is a hobby remake." + "\n" + "All intelectual material used by this programmer is copyrighted by Infocom." + "\n"
+                      + "Press any key to play...");
+
+        ConsoleKeyInfo key = Console.ReadKey();
+        Console.Clear();
 
         while (true)
         {
-            Console.SetCursorPosition(0, Console.WindowHeight - 1); // Set cursor position to the last row
-            Console.Write("> "); // And display the input prompt. It looks funny if you don't include the input line next.
+            UpdateStatusBar(score, moves);
+
+            // display the last n lines of the output buffer
+            int n = Console.WindowHeight - 3;
+            for (int i = Math.Max(bufferPos -n, 0); i < bufferPos; i++)
+            {
+                Console.WriteLine(outputBuffer[i % bufferSize]);
+            }
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.Write("> ");
 
             string input = Console.ReadLine();
 
-            // Clear the previous output before displaying new output
-            Console.SetCursorPosition(0, 3); // set cursor position to row 3
-            Console.Write(new string(' ', Console.WindowWidth)); // This clears row 3
-            Console.SetCursorPosition(0, 4); // And now we move the cursor to row 4 of the console
-            Console.Write(new string(' ', Console.WindowWidth)); // Clear row 4.
-
             if (input.ToLower() == "quit")
             {
+                Console.WriteLine("Goodbye!");
                 break;
             }
             else if (input.ToLower() == "help")
             {
-                Console.SetCursorPosition(0, 3);
-                Console.WriteLine("Commands: quit, help, look, go <direction>");
-                UpdateStatusBar(score, moves);
+                outputBuffer[bufferPos % bufferSize] = "Commands: quit, help, look, go <direction>";
+                bufferPos++;
             }
         }
     }
